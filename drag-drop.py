dfadd2069ad27af41a6ace1faed782f7da49b329
Mathis <ducher.mathis@gmail.com>
2025-01-07 20:00:04 +0100

    Automatisation de l'attribution des adresses IP en fonction de la structure du réseau (dans le JSON)

--- drag-drop.py
+++ drag-drop.py
@@ -1,8 +1,7 @@
 import json
 
 # Template de la configuration de base Cisco
-config_template = """
-!
+config_template = """!
 !
 !
 service timestamps debug datetime msec
@@ -30,44 +29,71 @@ line aux 0
  no login
 !
 !
-end
-"""
+end"""
+
 # Template pour la configuration des interfaces
 interface_template = """
 interface {int_name}
  ip address {int_ip}
- no shutdown
-"""
+ no shutdown"""
 
 # Charger le fichier JSON
 with open('config.json', 'r') as JSON:
     intent = json.load(JSON)
 
-# Fonction pour générer la configuration des interfaces
-def generate_interfaces_config(router_int):
-    int_config = ""
-    for interface in router_int:
-        int_config += interface_template.format(
-            int_name=interface['name'],
-            int_ip=interface['ip']
-        )
+# Fonction pour générer les adresses IPv6 des interfaces
+def generate_ipv6_address(router_id, neighbor_id):
+    # X est le plus petit router_id, Y est l'autre router_id à connecter
+    X = min(router_id, neighbor_id)
+    Y = max(router_id, neighbor_id)
+    return f"2001:{X}:{Y}::{router_id}/64"
+
+# Fonction pour générer la configuration d'une interface
+def generate_interface_config(router_id, neighbor_id, int):
+    int_config = interface_template.format(
+        int_name=int,
+        int_ip=generate_ipv6_address(router_id, neighbor_id)
+    )
     return int_config
 
+# Fonction pour générer toutes les configurations des interfaces de tous les routeurs selon les liens existants
+def interfaces_config(router_id):
+    all_int_config = {id: "" for id in router_id.values()} # Initialise les dictionnaires pour stocker la configuration des interfaces de tous les routeurs
+
+    # Collecte des liens entre les routeurs
+    for link in intent['reseau']:
+        router_name_X, int_X, router_name_Y, int_Y = link
+        router_X_id = router_id[router_name_X]
+        router_Y_id = router_id[router_name_Y]
+
+        all_int_config[router_X_id] += generate_interface_config(router_X_id, router_Y_id, int_X) # IP pour le routeur X
+        all_int_config[router_Y_id] += generate_interface_config(router_Y_id, router_X_id, int_Y) # IP pour le routeur Y
+
+    return all_int_config
+
 # Fonction pour générer la configuration de chaque routeur
-def generate_config(router_name, router_int):
-    int_config = generate_interfaces_config(router_int)
+def generate_config(router_name, all_int_config):
+    int_config = all_int_config[router_id[router_name]]
     return config_template.format(router_name=router_name, int_config=int_config)
+
+router_id = {} # Dictionnaire pour l'identification numérique des routeurs et les interfaces
+# Générer les identifiants de chaque routeur
+for i in range (1,len(intent['router'])+1):
+    router_name = intent['router'][i-1]['name']
+    router_id[router_name]=i
+
+# Récupérer les configurations des interfaces de tous les routeurs selon les liens existants
+all_int_config = interfaces_config(router_id)
 
 # Générer les fichiers de configuration pour chaque routeur
 for router in intent['router']:
     router_name = router['name']
-    router_int = router['interface']
-    
+
     # Générer la configuration pour chaque routeur
-    config = generate_config(router_name, router_int)
+    config = generate_config(router_name, all_int_config)
     
     # Sauvegarder la configuration dans un fichier
-    filename = f"{router_name}i1_startup-config.cfg"
+    filename = f"i{router_id[router_name]}_startup-config.cfg"
     with open(filename, 'w') as f:
         f.write(config)
 
