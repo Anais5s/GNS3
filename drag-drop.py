ef33c6e3847dd31ab0a38587f3fca18bde822f73
Mathis <ducher.mathis@gmail.com>
2025-01-10 10:08:36 +0100

    Modification format reseau (JSON) / ajout des interfaces loopback

--- drag-drop.py
+++ drag-drop.py
@@ -33,7 +33,9 @@ end"""
 # Template pour la configuration des interfaces
 interface_template = """
 interface {int_name}
+ no ip address
  ip address {int_ip}
+ ipv6 enable
  no shutdown"""
 
 # Charger le fichier JSON
@@ -41,32 +43,36 @@ with open('intent.json', 'r') as JSON:
     intent = json.load(JSON)
 
 # Fonction pour générer les adresses IPv6 des interfaces
-def generate_ipv6_address(router_id, neighbor_id):
+def generate_ipv6_address(router_id, neighbor_id, suffix):
     # X est le plus petit router_id, Y est l'autre router_id à connecter
     X = min(router_id, neighbor_id)
     Y = max(router_id, neighbor_id)
-    return f"2001:{X}:{Y}::{router_id}/64"
+    return f"2001:{X}:{Y}::{suffix}/64"
 
 # Fonction pour générer la configuration d'une interface
-def generate_interface_config(router_id, neighbor_id, int):
+def generate_interface_config(router_id, neighbor_id, suffix, int):
     int_config = interface_template.format(
         int_name=int,
-        int_ip=generate_ipv6_address(router_id, neighbor_id)
+        int_ip=generate_ipv6_address(router_id, neighbor_id, suffix)
     )
     return int_config
 
 # Fonction pour générer toutes les configurations des interfaces de tous les routeurs selon les liens existants
 def interfaces_config(router_id):
-    all_int_config = {id: "" for id in router_id.values()} # Initialise les dictionnaires pour stocker la configuration des interfaces de tous les routeurs
+    # all_int_config = {id: "" for id in router_id.values()} # Initialise les dictionnaires pour stocker la configuration des interfaces de tous les routeurs
+    all_int_config = {}
+    for id in router_id.values():
+        all_int_config[id] = generate_interface_config("0", "0", id, "Loopback0")
 
     # Collecte des liens entre les routeurs
     for link in intent['reseau']:
-        router_name_X, int_X, router_name_Y, int_Y = link
+        router_name_X, int_X = link[0]
+        router_name_Y, int_Y = link[1]
         router_X_id = router_id[router_name_X]
         router_Y_id = router_id[router_name_Y]
 
-        all_int_config[router_X_id] += generate_interface_config(router_X_id, router_Y_id, int_X) # IP pour le routeur X
-        all_int_config[router_Y_id] += generate_interface_config(router_Y_id, router_X_id, int_Y) # IP pour le routeur Y
+        all_int_config[router_X_id] += generate_interface_config(router_X_id, router_Y_id, router_X_id, int_X) # IP pour le routeur X
+        all_int_config[router_Y_id] += generate_interface_config(router_Y_id, router_X_id, router_Y_id, int_Y) # IP pour le routeur Y
 
     return all_int_config
 
