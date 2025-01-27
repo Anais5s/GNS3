8187f26f81e78e4981a1670fe7d8fb2aa44adcfb
Anais5s <anaisboisson@gmail.com>
2025-01-27 16:36:15 +0100

    telnet fonctionnel?

--- telnet_fichier_config.py
+++ telnet_fichier_config.py
@@ -5,12 +5,20 @@ import re
 
 # Liste des appareils (fichier de config et ports)
 DEVICES = [
-    {"file": "i1_startup-config.cfg","port": 5005},
-    {"file": "i2_startup-config.cfg","port": 5007},
-    {"file": "i3_startup-config.cfg", "port": 5000},
-    {"file": "i4_startup-config.cfg","port": 5001},
-    {"file": "i5_startup-config.cfg","port": 5002},
-    {"file": "i6_startup-config.cfg", "port": 5003},
+    {"file": "i1_startup-config.cfg","port": 5000},
+    {"file": "i2_startup-config.cfg","port": 5001},
+    {"file": "i3_startup-config.cfg", "port": 5002},
+    {"file": "i4_startup-config.cfg","port": 5003},
+    {"file": "i5_startup-config.cfg","port": 5004},
+    {"file": "i6_startup-config.cfg", "port": 5005},
+    {"file": "i7_startup-config.cfg","port": 5006},
+    {"file": "i8_startup-config.cfg","port": 5007},
+    {"file": "i9_startup-config.cfg", "port": 5008},
+    {"file": "i10_startup-config.cfg","port": 5009},
+    {"file": "i11_startup-config.cfg","port": 5010},
+    {"file": "i12_startup-config.cfg", "port": 5011},
+    {"file": "i13_startup-config.cfg", "port": 5012},
+    {"file": "i14_startup-config.cfg","port": 5013},
 ]
 
 # Charger le fichier JSON
@@ -26,28 +34,27 @@ def configure_device(CONFIG_FILE,port):
         tn = telnetlib.Telnet("127.0.0.1", port)
         print(f"Connexion établie avec 127.0.0.1:{port}")
         tn.read_until(b"Press RETURN to get started!",timeout=5)
-        time.sleep(5)
         tn.write(b"\r\n")
-        tn.write(b"enable\n")
-        tn.write(b"configure terminal\n")
-        time.sleep(5)
+        tn.write(b"enable"+b"\r\n")        
+        time.sleep(0.2)
+        tn.write(b"configure terminal\n"+b"\r\n")
+        time.sleep(0.2)
 
         for command in config_commands:
             command = command.strip()
             if command and command!="!": 
-                tn.write(command.encode('ascii') + b"\n")
+                tn.write(command.encode('ascii') + b"\r\n")
                 time.sleep(0.3)
                 output = tn.read_very_eager().decode('ascii')  # Lire la sortie
                 print(f"Commande : {command}, Réponse : {output}")
 
-        tn.write(b"end\n")
-        tn.write(b"write\n")
-        tn.write(b"exit\n")
+        tn.write(b"write"+b"\r\n")
+        time.sleep(0.3)
+        tn.write(b"y"+b"\r\n")
         print(f"Configuration terminée pour 127.0.0.1:{port}")
     except Exception as e:
         print(f"Erreur avec 127.0.0.1:{port} : {e}")
 
 # Configurer tous les appareils
-#for device in DEVICES:
-#    configure_device(device["file"],device["port"])
-configure_device(DEVICES[1]["file"],DEVICES[1]["port"])
+for device in DEVICES:
+    configure_device(device["file"],device["port"])
